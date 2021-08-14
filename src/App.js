83a98ecd63613147115ef0245a58437c5eb4337a
roua-dev <ghanmiroua7@gmail.com>
2021-08-14 22:36:35 +0100

    fi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { moviedata } from "./data";
+import NavBar from "./components/NavBar/NavBar";
+import MovieList from "./components/MovieList/MovieList";
+import AddMovie from "./components/AddMovie/AddMovie";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/NavBar/Home";
+import Login from "./components/NavBar/Login";
+import Details from "./components/Details/Details";
+
 
 function App() {
+  const [movielist, setMovielist] = useState(moviedata);
+  const [title, setTitle] = useState("");
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const [rate, setRate] = useState(0);
+  const ratingChanged = (newRating) => {
+    setRate(newRating);
+  }
+
+  const handleAdd = (newMovie) => {
+    setMovielist([...movielist, newMovie]);
+  }
+
+  //Delete movie from list of movies
+  const handleDelete = (id) => {
+    setMovielist(movielist.filter((movie) => movie.id !== id))
+  }
+
+  /* const routes = {
+    "/": () => <Home />,
+  "/about": () => <MovieList movies={movielist.filter((movie) => movie.title.toUpperCase().includes(title.toUpperCase()) && movie.rate >= rate)} handleDelete={handleDelete} />,
+  "/contact": () => <AddMovie handleAdd={handleAdd} />
+  } */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar title={title} handleChange={handleChange} rate={rate} ratingChanged={ratingChanged} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/movielist/:id" render={(props) => <Details {...props} mvlst={movielist} />} />
+        </Switch>
+        <MovieList movies={movielist.filter((movie) => movie.title.toUpperCase().includes(title.toUpperCase()) && movie.rate >= rate)} handleDelete={handleDelete} />
+        <AddMovie handleAdd={handleAdd} />
+      </Router>
     </div>
   );
 }
---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
+
+const MovieCard = ({ movie, handleDelete }) => {
+    return (
+        <div>
+            <Card style={{ width: '19rem' }} className="mx-4 my-3">
+                <Card.Img variant="top" src={movie.posterUrl} style={{ height: "500px" }} />
+                <Card.Body>
+                    <Card.Title>{movie.title}</Card.Title>
+                    <Card.Text>
+                        {movie.description}
+                    </Card.Text>
+                    <Card.Text>
+                        <ReactStars
+                            count={5}
+                            size={24}
+                            activeColor="#ffd700"
+                            isHalf={true}
+                            value={movie.rate}
+                        />
+                    </Card.Text>
+                    <div className="d-flex justify-content-around">
+                        <Button variant="primary" onClick={() => handleDelete(movie.id)}>Delete</Button>
+                        <Link to={`/movielist/${movie.id}`}><Button variant="primary">See Details</Button></Link>
+                    </div>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+
+export default MovieCard
